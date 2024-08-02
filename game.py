f7aea031becb4e6aae30b253db519f87f1b58ef0
Ravindra Prajapati <ravindra143882@gmail.com>
2024-08-02 11:02:44 +0530

    game.py

--- game.py
+++ game.py
@@ -16,51 +16,51 @@ while True:
     1.Yes
     2.No| Exit
     '''))
-    if uc == 1: 2
-
-    for a in range(1, 6):
-        usrIn = int(input('''
+    if uc == 1:
+        for a in range(1, 6):
+            usrIn = int(input('''
             1 Rock
             2 Scissor
             3 Paper
             '''))
-        if usrIn == 1:
-            uchoice = 'rock'
-        elif usrIn == 2:
-            uchoice = 'scissor'
-        elif usrIn == 3:
-            uchoice = 'paper'
-        Cchoice = random.choice(l)
-        if Cchoice == uchoice:
-            print('Computer value:-', Cchoice)
-            print('User value:-', uchoice)
-            print('Game Draw')
-            Ucount = Ucount + 1
-            Ccount = Ccount + 1
-        elif ((uchoice == 'rock' and Cchoice == 'scissor')
-              or (uchoice == 'paper' and Cchoice == 'rock')
-              or (uchoice == 'scissor' and Cchoice == 'paper')):
-            print('Computer value:-', Cchoice)
-            print('User value:-', uchoice)
-            print('you win')
-            Ucount = Ucount + 1
+            if usrIn == 1:
+                uchoice = 'rock'
+            elif usrIn == 2:
+                uchoice = 'scissor'
+            elif usrIn == 3:
+                uchoice = 'paper'
+            Cchoice = random.choice(l)
+            if Cchoice == uchoice:
+                print('Computer value:-', Cchoice)
+                print('User value:-', uchoice)
+                print('Game Draw')
+                Ucount = Ucount + 1
+                Ccount = Ccount + 1
+            elif ((uchoice == 'rock' and Cchoice == 'scissor')
+                  or (uchoice == 'paper' and Cchoice == 'rock')
+                  or (uchoice == 'scissor' and Cchoice == 'paper')):
+                print('Computer value:-', Cchoice)
+                print('User value:-', uchoice)
+                print('you win')
+                Ucount = Ucount + 1
+            else:
+                print('computer value:-', Cchoice)
+                print('user value:-', uchoice)
+                print('computer win')
+                Ccount = Ccount + 1
+        if Ucount == Ccount:
+            print('Game Draw...')
+            print('User Score', Ucount)
+            print('Computer Score', Ccount)
+        elif Ucount > Ccount:
+            print('Barvo!!..You have won...')
+            print('User Score', Ucount)
+            print('Computer Score', Ccount)
         else:
-            print('computer value:-', Cchoice)
-            print('user value:-', uchoice)
-            print('computer win')
-            Ccount = Ccount + 1
-    if Ucount == Ccount:
-        print('User Score', Ucount)
-        print('Computer Score', Ccount)
-        print('final Resul:-' + 'Game Draw...')
-    elif Ucount > Ccount:
-        print('User Score', Ucount)
-        print('Computer Score', Ccount)
-        print('Final Result:-' + 'Barvo!!..You have won...')
+            print('Computer has Won...')
+            print('User Score', Ucount)
+            print('Computer Score', Ccount)
+
+        pass
     else:
-        print('User Score', Ucount)
-        print('Computer Score', Ccount)
-        print('Final Result:-' + 'Computer has Won')
-    pass
-else:
-    break
+        break
